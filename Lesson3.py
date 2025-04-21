18d2d3f13cb2311f068e2ba9bfd9e02558177a06
Tanya <tveremiiets@gmail.com>
2025-04-21 19:49:50 +0300

    lesson3

---
+++ Lesson3.py
@@ -0,0 +1,8 @@
+some_string ='My name Tanya'
+
+upper = some_string.upper() #
+lower = some_string.lower() #
+title = some_string.title() #
+capitalize = some_string.capitalize() #
+chain = some_string.lower().upper().capitalize()
+
