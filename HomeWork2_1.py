8e53f98f9334ee0f19e4d261fc54fb27681d8deb
Tanya <tveremiiets@gmail.com>
2025-04-27 12:26:21 +0300

    home work lesson 2

--- HomeWork2_1.py
+++ HomeWork2_1.py
@@ -1,7 +1,20 @@
-home_field = 9776
-print(divmod(9,8))
-div, mod = (9,8)
+from unicodedata import digit
+
+#home_field = int(input("7498"))
+#home_field2=1000
+#digit=home_field
+#left,right=divmod(digit,home_field2)
+#print(left)
+
+home_field=int(input(7498))
+res1=home_field//1000
+res2=(home_field//100)%10
+res3=(home_field%100)//10
+res4=home_field%10
+print(res1)
+print(res2)
+print(res3)
+print(res4)
 
 
-print(div)
-print(mod)
+
--- HomeWork2_2.py
+++ HomeWork2_2.py
@@ -1,5 +1,9 @@
+#variant1
 HW_number = 98765
 HW_number_mir =int(str(HW_number)[::-1])
-
 print(HW_number)
 print(HW_number_mir)
+
+#variant2
+HW_number = 98765
+HW_number.sort(reverse=True)
