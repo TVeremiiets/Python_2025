c552594a5e4827da11e4d3249c78638d2631f2d7
Tanya <tveremiiets@gmail.com>
2025-04-27 12:51:52 +0300

    home work lesson 2

--- HomeWork2_2.py
+++ HomeWork2_2.py
@@ -5,6 +5,5 @@ HW_number_mir =int(str(HW_number)[::-1])
 print(HW_number)
 print(HW_number_mir)
 
-#variant2
-HW_number =98765
-print(list(reversed(HW_number)))
+#HW_number =98765
+#print(list(reversed(HW_number)))
