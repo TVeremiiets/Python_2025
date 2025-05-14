84cbbad7a94d611cd4f592facca75d64f6c03e20
Tanya <tveremiiets@gmail.com>
2025-05-14 20:09:45 +0300

    Added Homework3

--- hashtag.py
+++ hashtag.py
@@ -1,10 +1,16 @@
 from re import sub
+import string
+from shlex import join
 
 my_row='Python Community'
-#print(my_row)
+print(my_row)
 #my_row='i like python community!'
 #my_row='Should, I. subscribe? Yes!'
 
-print(my_row)
+#variant1
 print('#' + sub(r'(?:^|\W+)(\w)?', lambda x: x.group(1).upper() if x.group(1) else '', my_row)[:140])
+#print('#' + ''.join(map(lambda x: join(filter(str.isalnum, my_row)).capitalize(), input().split()))[:140])
 
+#variant2
+print('#' + ''.join(map(lambda x: x.capitalize(), ''.join(filter(str.isalnum, my_row)).split()))[:140])
+print('#' + ''.join(map(lambda x: x.upper(), ''.join(filter(str.isalnum, my_row)).split()))[:140])
