c2ffec584c4c1d0cee090e7b70ae0e289f1200a3
Tanya <tveremiiets@gmail.com>
2025-05-18 16:48:35 +0300

    Added Homework3

--- say_hi.py
+++ say_hi.py
@@ -3,7 +3,7 @@ def say_hi(name: str, age: int) -> str:
 
 assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
 assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
-print('test passed')
+print('OK')
 
 
 #user_name = input("Enter your name: ")
