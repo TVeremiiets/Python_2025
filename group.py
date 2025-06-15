e2a6e10136ee7a483b8fc0f9cb63796ee0a771db
Tanya <tveremiiets@gmail.com>
2025-06-15 22:27:51 +0300

    Added Homework3

---
+++ group.py
@@ -0,0 +1,31 @@
+from student import Student
+
+class GroupFullException(Exception):
+    def __init__(self, message="Group is full. Cannot add more than 10 students."):
+        super().__init__(message)
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.group = set()
+
+    def add_student(self, student):
+        if len(self.group) >= 10:
+            raise GroupFullException()
+        if isinstance(student, Student):
+            self.group.add(student)
+
+    def find_student(self, last_name):
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.group)
+        return f'Number: {self.number}\n{all_students}'
